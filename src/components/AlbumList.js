4e927f0ebc3a4957b759b8bd3afadf2d0df80eb9
Anhad Gill <anhad@frogplum.com>
2019-05-07 16:58:19 -0700

    Changed a functional component to a class based component.

---
+++ src/components/AlbumList.js
@@ -0,0 +1,16 @@
+// import libraries for making a component
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+// Make a component
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
