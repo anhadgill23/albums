3f79f79d08ab967bfae0509a9c6da6707ab09d14
Anhad Gill <anhad@frogplum.com>
2019-05-08 12:11:58 -0700

    render a new component and fetches data from an api to display.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,28 @@
 // import libraries for making a component
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
 
 // Make a component
 class AlbumList extends Component {
+
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
