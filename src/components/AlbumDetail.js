166a4118b676e2f72cef9625cea924a4f03a062a
Anhad Gill <anhad@frogplum.com>
2019-05-16 13:59:27 -0700

    Hotfix.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Card from './Card';
 
 // ask whather it is a class based component or a functional component
 // Does it need lifecyccle methods or need access to state?
 
 const AlbumDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   );
 };
 
